4af726091f29641dc90cbb2140aba66d23f672f2
olegov-lab <olego8760@gmail.com>
2021-01-28 23:45:16 +0300

    test: brackets

--- src/index.js
+++ src/index.js
@@ -1,50 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-    let arr = [];
-    let open = true;
-    let len = str.length;
-    for(let i = 0; i < len; i++) {
-        if(pairClosed(str[i], bracketsConfig) != str[i]) {
-            if(isOpen(str[i], bracketsConfig) == true) arr.push(str[i]);
-            else {
-                if(arr.length != 0) {
-                    if(pairClosed(str[i], bracketsConfig) == arr[arr.length - 1]) arr.pop();
-                    else break;
-                }
-                else {                
-                    open = false; 
-                    break;
-                }
-            }
-        }
-        else {
-            if(arr.length != 0) {
-                if(str[i] == arr[arr.length - 1]) arr.pop();
-                else arr.push(str[i]);
-            }
-            else arr.push(str[i]);
-        }
-    }
-    if(arr.length > 0 || open ==  false) return false;
-    else return true;
-}
 
-function pairClosed(symb, bracketsConfig) {
-    let len = bracketsConfig.length;
-    let open='';
-    for(let i = 0; i < len; i++) {
-        if(symb == bracketsConfig[i][1]) open = bracketsConfig[i][0];
-    }
-    return open;    
-}
+    let decomposedConfig = [];
 
-function isOpen(symb, bracketsConfig) {
-    let len = bracketsConfig.length;
-    let exists = false;
-    for(let i = 0; i < len; i++) {
-        if(symb == bracketsConfig[i][0]) {
-            exists = true;
-            break;
-        }            
+    for (let config of bracketsConfig) {
+      decomposedConfig = decomposedConfig.concat(config)
     }
-    return exists;
-}
+    bracketsConfig = decomposedConfig;
+
+    let bracketIndex;
+    const stack = [];
+
+    for (let bracket of str) {
+      bracketIndex = bracketsConfig.indexOf(bracket);
+
+      if (bracketsConfig[bracketIndex] === bracketsConfig[bracketIndex +1]) {
+
+          if (stack[stack.length - 1] !== bracketIndex) {
+            stack.push(bracketIndex);
+          }else {
+            stack.pop()
+          }
+      } else {
+        if (bracketIndex % 2 === 0) {
+          stack.push(bracketIndex + 1)
+        } else {
+          if (stack.pop() !== bracketIndex) {
+            return false
+          }
+        }
+      }
+    }
+    return stack.length === 0
+  }
